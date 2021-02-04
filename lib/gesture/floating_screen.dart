67de52f4c78ae0bfb368dc0bacb1e83597a1269d
terry.li <>
2021-02-04 15:41:35 +0800

    Change widget construct style

--- lib/gesture/floating_screen.dart
+++ lib/gesture/floating_screen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
 class FloatingScreen extends StatefulWidget {
-  double width = 0.0;
-  FloatingScreen({Key key, this.width}) : super(key: key);
+  const FloatingScreen({Key key, @required this.width}) : super(key: key);
+
+  final double width;
 
   @override
   _FloatingScreenState createState() => _FloatingScreenState();
@@ -12,7 +13,6 @@ class _FloatingScreenState extends State<FloatingScreen> {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: (){},
       child: Container(
         width: widget.width,
         color: Colors.amber,
